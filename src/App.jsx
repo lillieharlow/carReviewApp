32a68946147555f2a04e172fab95d5504c480193
Lillie <chapman.lillie@gmail.com>
2025-12-17 11:46:08 +1100

    feat: App structure and route files created. App.jsx updated to list all routes needed for the challenge and linked to their created files.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom";
+
+import Layout from "./components/Layout.jsx";
+import Home from "./pages/Home.jsx";
+import CarDetails from "./pages/CarDetails.jsx";
+import NewCar from "./pages/NewCar.jsx";
+import NotFound from "./pages/NotFound.jsx";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cars/new" element={<NewCar />} />
+        <Route path="/cars/:id" element={<CarDetails />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Layout>
   );
 }
 
-export default App
+export default App;
